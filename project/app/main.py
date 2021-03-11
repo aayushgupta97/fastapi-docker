6fb34bce35bdb4b11d8e0d0247db26f2a4829990
Aayush Gupta <aayushgupta2097@gmail.com>
2021-03-12 05:17:19 +0530

    initial commit

---
+++ project/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Depends
+from app.config import Settings, get_settings
+app = FastAPI()
+
+
+@app.get("/ping")
+async def ping(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong",
+        "environment": settings.environment,
+        "testings": settings.testing
+        }
