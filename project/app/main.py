83578e53a9da1676e11aeb7eb167077dffbc6268
Aayush Gupta <aayushgupta2097@gmail.com>
2021-03-13 01:24:15 +0530

    added aerich to tortoise for migrations

--- project/app/main.py
+++ project/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 register_tortoise(
     app,
     db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
+    modules={"models": ["app.models.tortoise", "aerich.models"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
